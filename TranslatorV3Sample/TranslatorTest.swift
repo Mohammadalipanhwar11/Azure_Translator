a297fe12116429b41747c28cad5a7ba73a793094
Mohammad Ali Panhwar <panhwarali11@gmail.com>
2023-06-12 23:13:34 +0500

    updated code

---
+++ TranslatorV3Sample/TranslatorTest.swift
@@ -0,0 +1,32 @@
+//
+//  TranslatorTest.swift
+//  TranslatorV3Sample
+//
+//  Created by Mac on 23/05/2023.
+//  Copyright © 2023 MSTranslatorMac. All rights reserved.
+//
+
+import XCTest
+
+class TranslatorTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssertTrue(false)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
